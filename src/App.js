ee79bca010ffd208af070d28bd6b6469f3965d78
asusbit <hamzahalnofali37@gmail.com>
2025-03-21 12:16:08 +0400

    added better loading screen

--- src/App.js
+++ src/App.js
@@ -136,7 +136,7 @@ function App() {
 
   const [showModal, setShowModal] = useState(false) // init for showing form modal
 
-  if (loading) return <div className='font-silkscreen text-city-white'>Loading Crimes...<br/><span className='text-city-bright-red'>Please Wait, City Defender.</span></div> // showing loading instead of an incomplete screen
+  if (loading) return <div className='font-silkscreen text-center justify-center items-center flex text-3xl text-city-white'><p>Loading Crimes...<br/><span className='text-city-bright-red'>Please Wait, City Defender.</span></p></div> // showing loading instead of an incomplete screen
   if (error){
     return <>Error: {error.message}</> // showing an error message instead of an incomplete screen
   }
